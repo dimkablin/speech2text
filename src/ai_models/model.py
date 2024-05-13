8bfd32325d1d1b0ad5b5b50466c9cd232e9cb071
Dimka <dmitryellison@gmail.com>
2024-05-13 11:58:50 +0300

    bugfixing/fixed recognition of a long audio

--- src/ai_models/model.py
+++ src/ai_models/model.py
@@ -6,7 +6,7 @@ from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 from huggingface_hub.utils._validators import HFValidationError
 
 from ai_models.speech2text_interface import Speech2TextInterface
-from utils.features_extractor import load_audio
+from utils.features_extractor import load_audio, split_audio
 from env import USE_CUDA, MODEL_NAME
 
 
@@ -21,7 +21,7 @@ class Speech2text(Speech2TextInterface):
         self.model_name = model_name
         self.language = language
         self.device = device
-        self.path_to_model = "./ai_models/weigths/"
+        self.path_to_model = "./ai_models/weights/"
         self.torch_dtype = torch.float32
 
         if USE_CUDA is True and torch.cuda.is_available():
@@ -44,28 +44,31 @@ class Speech2text(Speech2TextInterface):
                 path,
                 torch_dtype=self.torch_dtype,
                 low_cpu_mem_usage=True,
-                use_safetensors=True
+                use_safetensors=True,
+                force_download=True
             )
 
             self.processor = AutoProcessor.from_pretrained(
                 path,
                 language=self.language,
-                task="transcribe"
+                task="transcribe",
+                force_download=True
             )
-
         # if we didnt find the model, we try to download it
-        except HFValidationError:
+        except (HFValidationError, OSError):
 
             # load the model
             self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
                 self.model_name,
                 torch_dtype=self.torch_dtype,
                 low_cpu_mem_usage=True,
-                use_safetensors=True
+                use_safetensors=True,
+                force_download=True
             )
             self.processor = AutoProcessor.from_pretrained(
                 self.model_name,
-                language=self.language
+                language=self.language,
+                force_download=True
             )
 
             #  save the model
@@ -81,29 +84,37 @@ class Speech2text(Speech2TextInterface):
         Returns:
             str: model output.
         """
-        # load the vois from path with 16gHz
-        input_features = self.processor(
-            load_audio(audio),
-            sampling_rate=16000,
-            return_tensors="pt"
-        ).input_features
 
-        input_features = input_features.to(self.device, dtype=self.torch_dtype)
+        inputs = load_audio(audio)
+        inputs_chunks = split_audio(inputs, sample_rate=16000)
 
-        # get decoder for our language
-        forced_decoder_ids = self.processor.get_decoder_prompt_ids(
-            language=self.language,
-            task="transcribe"
-        )
+        transcriptions = []
 
-        # model inference
-        pred_ids = self.model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
+        for chunk in inputs_chunks:
+            # load the vois from path with 16gHz
+            input_features = self.processor(
+                chunk,
+                sampling_rate=16000,
+                return_tensors="pt"
+            ).input_features
 
-        # decode the transcription
-        transcription = self.processor.batch_decode(pred_ids, skip_special_tokens=True)
-        input_features.to("cpu")
+            input_features = input_features.to(self.device, dtype=self.torch_dtype)
 
-        return transcription
+            # get decoder for our language
+            forced_decoder_ids = self.processor.get_decoder_prompt_ids(
+                language=self.language,
+                task="transcribe"
+            )
+
+            # model inference
+            pred_ids = self.model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
+
+            # decode the transcription
+            transcription = self.processor.batch_decode(pred_ids, skip_special_tokens=True)[0]
+            input_features.to("cpu")
+
+            transcriptions.append(transcription)
+        return "".join(transcriptions)
 
     def __str__(self) -> str:
         return self.model_name
--- src/utils/features_extractor.py
+++ src/utils/features_extractor.py
@@ -22,3 +22,25 @@ def load_audio(path: str | PathLike) -> torch.Tensor:
     resampler = torchaudio.transforms.Resample(sr, 16000)
     speech = resampler(speech)
     return speech.squeeze()
+
+def split_audio(audio: torch.Tensor, sample_rate: int, chunk_size_sec=30) -> torch.Tensor:
+    # вычисляем сколько чанков будет
+    chunk_size = sample_rate * chunk_size_sec
+    num_chunks = audio.size(0) // chunk_size
+    chunks = []
+
+    # проходимся по всем чанкам 
+    for i in range(num_chunks):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size
+        chunk = audio[start:end]
+        chunks.append(chunk)
+
+    # добавляем последний чанк
+    last_chunk = audio[num_chunks * chunk_size:]
+    # если у нас так вышло, что audio % (sample_rate * chunk_size_sec) == 0 
+    # то скипаем последний чанк
+    if len(last_chunk) > 0:
+        chunks.append(last_chunk)
+
+    return chunks
